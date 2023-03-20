15f0accb5c6594eddd400621ab5f54df5233e0b3
ritasidorska <m.sidorskaya@innopolis.university>
2023-03-20 11:09:46 +0300

    HW5

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<nav>
+    <a href="/">Home</a>
+    <a href="/bio">Bio</a>
+    <a href="/photo">Photo</a>
+</nav>
+
+<slot></slot>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,71 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<!--<script lang="ts">-->
+<!--    import Joke from "../../components/+page.svelte";-->
+<!--    import Bio from "../../components/+bio.svelte";-->
+<!--    import Projects from "../../components/+projects.svelte";-->
+<!--</script>-->
+
+<main>
+    <header>
+        <div>
+            <h1>Margarita Sidorskaya</h1>
+        </div>
+    </header>
+
+    <div class="bio+photo">
+<!--        <div class="child"><Joke email={"m.sidorskaya@innopolis.university"}/></div>-->
+<!--        <div class="child"><Bio/></div>-->
+<!--        <div class="child">Joke</div>-->
+        <a href="/about" class="child">Joke</a>
+<!--        <div class="child">Joke</div>-->
+        <div class="child">Bio</div>
+    </div>
+
+    <div class="bio+photo">
+<!--        <div class="child"><Projects/></div>-->
+        <div class="child">Projects</div>
+<!--        <div class="child"><img src="assets/me2.jpg" alt="Black and white photo of me"></div>-->
+    </div>
+
+<!--    <div style="display: flex">-->
+<!--        <a href="https://github.com/Klemencya" class=" fa fa-github-square fa-2x" id="gh-link" style="padding-left: 100px"></a>-->
+<!--        <a href="https://t.me/RitaSidorskaya" class="fa fa-telegram fa-2x" id="tg-link"></a>-->
+<!--        <a href="https://instagram.com/klementsiya?igshid=YmMyMTA2M2Y=" class="fa fa-instagram fa-2x"></a>-->
+<!--        <a href="mailto:m.sidorskaya@innopolis.university" class="fa fa-envelope fa-2x" id="email"></a>-->
+<!--    </div>-->
+</main>
+
+<style>
+    header {
+        text-align: center;
+        margin: auto;
+        width: 50%;
+    }
+
+    header div {
+        margin: 150px 150px 50px;
+    }
+
+    img {
+        width: 100%;
+    }
+
+    .bio\+photo {
+        display: flex;
+    }
+
+    .child {
+        text-align: center;
+        margin: 50px 100px;
+        width: 100%;
+    }
+
+    .fa {
+        padding: 20px;
+        font-size: 30px;
+        width: 50px;
+        text-align: center;
+        text-decoration: none;
+        border-radius: 50%;
+        color: black;
+    }
+</style>
